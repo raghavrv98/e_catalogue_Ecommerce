611d17aa593c2a3458c079c3ff0f29ada273f0e5
Raghav <raghav.agrawal@83incs.com>
2020-08-29 02:33:45 +0530

    adding login page, product page, updation on index, about us page, adding mail service in contact form, updating order and images, mysql connection setiup

--- controllers/homeCtrl.js
+++ controllers/homeCtrl.js
@@ -1,6 +1,37 @@
+var util = require('util')
+var databaseUtils = require('./../utils/databaseUtils')
+var mysql = require('mysql')
+var mailUtils = require('./../utils/mail-utils')
+var connection = mysql.createConnection({
+    //properties
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'ecommerce'
+});
 module.exports={
     showHome: (req, res, next)=>{
-        res.render('index', { title: 'Express' });
+        res.render('index');
+    },
+
+    postLogin: (req, res, next) => {
+        var email = req.body.emailId
+        var password = req.body.password
+        connection.query("select * from logincred", function(error, rows, fields){
+            if(!!error){
+                console.log('Error in Query');
+            }
+            else{
+                if(rows[0].emailId === email && rows[0].password === password){
+                    console.log('login Successful')
+                    res.render('index', {'loginError': false});
+                }
+                else{
+                    res.render('contact', { 'loginError': true});
+                    console.log('loginError')
+                }
+            }
+            });
     },
 
     showContact: (req, res, next)=>{
@@ -9,5 +40,24 @@ module.exports={
 
     showAboutUs: (req, res, next)=>{
         res.render('aboutUs', { title: 'Express' });
+    },
+
+    showLogin: (req, res, next)=>{
+        res.render('login', { title: 'Express' });
+    },
+
+    showProducts: (req, res, next)=>{
+        res.render('products', { title: 'Express' });
+    },
+    
+    enquiryMail: (req, res, next) => {
+        var name = req.body.name
+        var email = req.body.emailId
+        var message = req.body.message
+
+        var messageBody = "\n Name : " + name + "\n Email-id : " + email + "\n Message : "+ message
+        mailUtils.sendMail('teamletscipher@gmail.com', "Enquiry Mail", messageBody)
+        res.render('contact', {
+        });
     },
 }
--- routes/appRoutes.js
+++ routes/appRoutes.js
@@ -4,7 +4,11 @@ var homeCtrl=require('../controllers/homeCtrl')
 
 /* GET home page. */
 router.get('/', homeCtrl.showHome);
+router.get('/products', homeCtrl.showProducts);
+router.get('/login', homeCtrl.showLogin);
+router.post('/login', homeCtrl.postLogin);
 router.get('/contact', homeCtrl.showContact);
 router.get('/aboutUs', homeCtrl.showAboutUs);
+router.post('/contact', homeCtrl.enquiryMail);
 
 module.exports = router;
